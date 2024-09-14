bec3bf44a98d8a50723c2ca401f4a0f5ff02d25c
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-09-14 19:56:36 -0300

    Finalização da parte de introdução, texto e imagem

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './intro.css'
+import React from 'react';
+import './intro.css';
+import bg from '../../assets/image.png';
+import btnImg from '../../assets/hireme.png'
+import { Link } from 'react-scroll'
 
-const intro = () => {
+const Intro = () => {
     return (
-        <div>
-
-        </div>
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Olá,</span>
+                <span className="introText">Eu sou <span className="introName">Emily</span><br />Estudante de Desenvolvimento</span>
+                <p className="introPara">Lorem ipsum dolor, sit amet. Dolorum voluptates eum atque voluptate non magni qui,<br /> id ex obcaecati assumenda aliquam sunt.</p>
+                <Link><button className="btn"><img src={btnImg} alt="Me contrate" className='btnImg' />Me contrate</button></Link>
+            </div>
+            <img src={bg} alt="Profile" className="bg" />
+        </section>
     )
 }
 
-export default intro
+export default Intro;
