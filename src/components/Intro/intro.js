6022359236f997e1d8cceb6961a80273d815ebf0
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-12-02 17:01:30 -0300

    ajustes na intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,12 +9,12 @@ const Intro = () => {
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Olá,</span>
-                <span className="introText">Eu sou <span className="introName">Emily</span><br />Estudante de Desenvolvimento</span>
-                <p className="introPara">Lorem ipsum dolor, sit amet. Dolorum voluptates eum atque voluptate non magni qui,<br /> id ex obcaecati assumenda aliquam sunt.</p>
+                <span className="introText">Eu sou <span className="introName">Emily, </span><br />Desenvolvedora</span>
+                <p className="introPara"> com 1 ano de experiência em projetos front-end e back-end, especializada em JavaScript, CSS, HTML e React. Atualmente, curso Análise e Desenvolvimento de Sistemas, unindo conhecimento acadêmico a habilidades práticas, como desenvolvimento de soluções customizadas no Wix, integrações com APIs, geração de PDFs, construção de chatbots e manipulação de dados. Minha trajetória também inclui forte background em gestão de eventos, destacando minha versatilidade e foco em resultados.</p>
                 <Link><button className="btn" onClick={() => {
                     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
                 }}>
-                    <img src={btnImg} alt="Me contrate" className='btnImg' />Me contrate</button></Link>
+                    <img src={btnImg} alt="Me contrate" className='btnImg' />Entre em contato</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>
